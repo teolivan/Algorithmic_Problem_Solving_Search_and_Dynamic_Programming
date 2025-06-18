c5a1833a8ce1248db7bc7ce9ffdb9a2e2bb6e20b
olivia <oliviateoliva@gmail.com>
2025-06-18 18:54:13 +0200

    start knapsack problem

---
+++ Knapsack_problem_A.cpp
@@ -0,0 +1,20 @@
+// Created by olivi on 2025-06-18.
+/*
+Implement a solution to the classic knapsack problem. You are given a knapsack that can hold up to a certain weight
+(its capacity), and several items you may choose to put in the knapsack. Each item has a weight and a value. Choose a
+subset of the items (which could be all of them, or none of them) having the greatest value that fit into the knapsack
+(i.e. the sum of the weights of the items you choose must be less than or equal to the knapsack capacity).
+
+Input
+The input consists of between 1 and 30 test cases. Each test case begins with an integer 1 ≤ C ≤ 2000, giving the
+capacity of the knapsack, and an integer 1 ≤ n ≤ 2000, giving the number of objects. Then follow n lines, each giving
+the value and weight of the n objects. Both values and weights are integers between 1 and 10,000.
+
+Output
+For each test case, output a line containing the number of items chosen, followed by a line containing the indices of
+the chosen items (the first item has index 0, the second index 1, and so on). The indices can be given in any order.
+*/
+
+#include "Knapsack_problem_A.h"
+
+
---
+++ Knapsack_problem_A.h
@@ -0,0 +1,16 @@
+//
+// Created by olivi on 2025-06-18.
+//
+
+#ifndef KNAPSACK_PROBLEM_A_H
+#define KNAPSACK_PROBLEM_A_H
+
+
+
+class Knapsack_problem_A {
+
+};
+
+
+
+#endif //KNAPSACK_PROBLEM_A_H
